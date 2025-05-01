2638f440b3624b5c85ebaeb0b934c1541595bd85
luiguilherme <104606366+luiguilherme@users.noreply.github.com>
2025-05-01 16:53:17 -0300

    tela nova senha

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import TelaInicio from './pages/aluno/telaInicio/TelaInicio';
 import Register from './pages/aluno/telaCadastro/Register';
 import TelaLogin from './pages/aluno/telaLogin/LoginPage';
 import ConfirmaEmail from './pages/aluno/confirmaEmail/confirmaEmail';
+import NovaSenhaPage from './pages/aluno/novaSenha/novaSenhaPage';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<TelaLogin />} />
         <Route path="/confirma-email" element={<ConfirmaEmail />} />
+        <Route path="/novaSenha" element={<NovaSenhaPage />} />
 
       </Routes>
     </Router>
--- front/src/pages/aluno/telaLogin/LoginPage.js
+++ front/src/pages/aluno/telaLogin/LoginPage.js
@@ -7,9 +7,12 @@ const LoginPage = () => {
   const [senha, setSenha] = useState('');
   const [mensagem, setMensagem] = useState('');
   const navigate = useNavigate();
+  const [formData] = useState({
+      email: ""
+    });
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://suaapi.com/login', {
+      const response = await fetch('http://localhost:8080/nexttalents/student/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, senha }),
@@ -28,19 +31,36 @@ const LoginPage = () => {
     }
   };
 
+  const handleForgtPass = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/nexttalents/student/forgot-pass ', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        setMensagem('se existir cadastro um email sera enviado');
+      } else {
+        setMensagem('Erro no envio do email.');
+      }
+    } catch (err) {
+      setMensagem('Erro de conexão com o servidor.');
+    }
+  };
+
   return (
     <div className="login-container">
-      <header className="header">
-        <img src="/logo-nex-talents.png" alt="Nex.Talents" className="logo" />
-      </header>
+    <img src="/logo-nex-talents.png" alt="Nex.Talents" className="logo" />
       <div className="login-box">
         <h2>Login</h2>
         <label>Email:</label>
         <input
           type="email"
           placeholder="seunome@email.com"
-          value={email}
+          value={formData.email}
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
         <label>Senha:</label>
         <input
@@ -48,12 +68,13 @@ const LoginPage = () => {
           placeholder="********"
           value={senha}
           onChange={(e) => setSenha(e.target.value)}
+          required
         />
         <button onClick={handleLogin}>Acessar</button>
         <div className="cadastro" onClick={() => navigate("/register")} style={{ cursor: "pointer" }}>
         <p className="cadastro">Não possui login?</p>
           </div>
-        <p className="esqueciSenha">Esqueceu a senha?</p>
+        <p className="esqueciSenha" onClick={handleForgtPass}>Esqueceu a senha?</p>
         {mensagem && <p className="mensagem">{mensagem}</p>}
       </div>
     </div>
